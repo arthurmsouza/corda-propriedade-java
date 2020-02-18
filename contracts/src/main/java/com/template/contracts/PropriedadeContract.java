bb2ae35ba04349a7139006a2c7fa1d54a19daf4d
Arthur <arthur.msouza@gmail.com>
2020-02-18 18:59:07 -0300

    add

---
+++ contracts/src/main/java/com/template/contracts/PropriedadeContract.java
@@ -0,0 +1,68 @@
+package com.template.contracts;
+
+import com.template.schema.PropertyDetails;
+import com.template.states.PropriedadeState;
+import net.corda.core.contracts.CommandData;
+import net.corda.core.contracts.CommandWithParties;
+import net.corda.core.contracts.Contract;
+import net.corda.core.transactions.LedgerTransaction;
+
+import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;
+import static net.corda.core.contracts.ContractsDSL.requireThat;
+
+// ************
+// * Contract *
+// ************
+public class PropriedadeContract implements Contract {
+    // This is used to identify our contract when building a transaction.
+    public static final String ID = "com.template.contracts.PropriedadeContract";
+
+    // A transaction is valid if the verify() function of the contract of all the transaction's input and output states
+    // does not throw an exception.
+    @Override
+    public void verify(LedgerTransaction tx) throws IllegalArgumentException {
+        CommandWithParties<PropertyCommand> command =
+                requireSingleCommand(tx.getCommands(), PropertyCommand.class);
+        PropertyCommand commandType = command.getValue();
+
+        if( commandType instanceof PropertyCommand.Criar) verificaCriar(tx,command);
+        else if (commandType instanceof  PropertyCommand.Transferir) verificarTrasferir(tx,command);
+        else if (commandType instanceof PropertyCommand.AprovacaoBanco) verificarAprocaoBanco(tx,command);
+    }
+
+    private void verificarAprocaoBanco(LedgerTransaction tx, CommandWithParties<PropertyCommand> command) {
+        requireThat(require -> {
+            return null;
+        });
+    }
+
+    private void verificarTrasferir(LedgerTransaction tx, CommandWithParties<PropertyCommand> command) {
+        requireThat(require -> {
+            require.using("A propriedade  deve ter apenas um input",tx.getInputs()
+                    .size()==1);
+            require.using("A propriedade  deve ter apenas um output",tx.getOutputs()
+                    .size()==1);
+
+            final PropriedadeState in = tx.inputsOfType(PropriedadeState.class).get(0);
+            final PropriedadeState out = tx.outputsOfType(PropriedadeState.class).get(0);
+
+            return null;
+        });
+    }
+
+    private void verificaCriar(LedgerTransaction tx, CommandWithParties<PropertyCommand> command) {
+        requireThat(require -> {
+            require.using("A propriedade nao deve ter nenhum input",tx.getInputs()
+                    .isEmpty());
+            require.using("A propriedade deve ter apenas um output ",tx.getOutputs()
+                    .size() == 1);
+            final PropriedadeState out = tx.outputsOfType(PropriedadeState.class).get(0);
+            return null;
+        });
+    }
+
+    // Used to indicate the transaction's intent.
+    public interface Commands extends CommandData {
+        class Action implements Commands {}
+    }
+}
---
+++ workflows/src/main/java/com/template/flows/VerifySignAndFinaliseFlow.java
@@ -0,0 +1,8 @@
+package com.template.flows;
+
+import net.corda.core.transactions.TransactionBuilder;
+
+public class VerifySignAndFinaliseFlow {
+    public VerifySignAndFinaliseFlow(TransactionBuilder builder) {
+    }
+}
